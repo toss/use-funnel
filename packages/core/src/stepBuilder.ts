772f9e913e64c77ca9543026bd1cd88c38260753
Minukang <minukang5874@gmail.com>
2024-09-08 17:00:01 +0900

    fix(core): when `createFunnelSteps` extends with no option at initial, the context assigned never type.

--- packages/core/src/stepBuilder.ts
+++ packages/core/src/stepBuilder.ts
@@ -1,88 +1,67 @@
-export type FunnelStepOption<TContext, TMeta = never> = { meta?: TMeta } & (
-  | { guard: (data: unknown) => data is TContext }
-  | { parse: (data: unknown) => TContext }
-);
+export type FunnelStepGuardOption<TContext> = {
+  guard: (data: unknown) => data is TContext;
+};
+
+export type FunnelStepParseOption<TContext> = {
+  parse: (data: unknown) => TContext;
+};
+
+export type FunnelStepOption<TContext> = FunnelStepGuardOption<TContext> | FunnelStepParseOption<TContext>;
 
 export function funnelStepOptionIsGuard<TContext>(
-  option: FunnelStepOption<TContext, any>,
-): option is { guard: (data: unknown) => data is TContext } {
+  option: FunnelStepOption<TContext>,
+): option is FunnelStepGuardOption<TContext> {
   return 'guard' in option && typeof option.guard === 'function';
 }
 
 export function funnelStepOptionIsParse<TContext>(
-  option: FunnelStepOption<TContext, any>,
-): option is { parse: (data: unknown) => TContext } {
+  option: FunnelStepOption<TContext>,
+): option is FunnelStepParseOption<TContext> {
   return 'parse' in option && typeof option.parse === 'function';
 }
 
-type FunnelStepMap = Record<string, FunnelStepOption<any, any> | undefined>;
+type FunnelStepMap = Record<string, FunnelStepOption<any> | undefined>;
 
-class FunnelStepBuilder<
+class SimpleFunnelStepBuilder<
   TContext,
-  TMeta,
   TStepMap extends FunnelStepMap = {},
-  TPrevFunnelStepOption extends FunnelStepOption<any, any> = never,
+  TPrevFunnelStepOption extends FunnelStepGuardOption<TContext> = FunnelStepGuardOption<TContext>,
 > {
   private stepMap: FunnelStepMap = {};
-  private prevFunnelStepOption: FunnelStepOption<any, any> | undefined;
+  private prevFunnelStepOption: FunnelStepGuardOption<any> | undefined;
 
-  constructor(private meta?: TMeta) {}
   extends<TName extends string>(
     steps: TName | TName[],
-  ): FunnelStepBuilder<
+  ): SimpleFunnelStepBuilder<
     TContext,
-    TMeta,
     TStepMap & {
       [K in TName]: TPrevFunnelStepOption;
     },
     TPrevFunnelStepOption
   >;
-  extends<
-    TName extends string,
-    TFunnelStepOption extends FunnelStepOption<any, any>,
-    TNewContext extends TFunnelStepOption extends FunnelStepOption<infer TContext, any> ? TContext : never,
-    TNewMeta extends TFunnelStepOption extends FunnelStepOption<any, infer TMeta> ? TMeta : never,
-  >(
-    steps: TName | TName[],
-    option: TFunnelStepOption | ((meta: TMeta) => TFunnelStepOption),
-  ): FunnelStepBuilder<
-    TNewContext extends unknown ? TContext : TNewContext,
-    TNewMeta extends unknown ? TMeta : TNewMeta,
-    TStepMap & {
-      [K in TName]: TFunnelStepOption;
-    },
-    TFunnelStepOption
-  >;
+
   extends<TName extends string, TRequiredKeys extends keyof TContext>(
     steps: TName | TName[],
     options: { requiredKeys: TRequiredKeys[] | TRequiredKeys },
-  ): FunnelStepBuilder<
+  ): SimpleFunnelStepBuilder<
     Omit<TContext, TRequiredKeys> & Pick<Required<TContext>, TRequiredKeys>,
-    TMeta,
     TStepMap & {
-      [K in TName]: FunnelStepOption<Omit<TContext, TRequiredKeys> & Pick<Required<TContext>, TRequiredKeys>, TMeta>;
+      [K in TName]: FunnelStepGuardOption<Omit<TContext, TRequiredKeys> & Pick<Required<TContext>, TRequiredKeys>>;
     },
-    FunnelStepOption<Omit<TContext, TRequiredKeys> & Pick<Required<TContext>, TRequiredKeys>, TMeta>
+    FunnelStepGuardOption<Omit<TContext, TRequiredKeys> & Pick<Required<TContext>, TRequiredKeys>>
   >;
-  extends(steps: string | string[], option?: unknown) {
-    let funnelStep: FunnelStepOption<any, any> | undefined;
-    if (isRequiredKeys(option)) {
-      const requiredKeys = [
-        ...(isRequiredKeys(this.meta)
-          ? Array.isArray(this.meta.requiredKeys)
-            ? this.meta.requiredKeys
-            : [this.meta.requiredKeys]
-          : []),
-        ...(isRequiredKeys(option)
-          ? Array.isArray(option.requiredKeys)
-            ? option.requiredKeys
-            : [option.requiredKeys]
-          : []),
-      ];
+
+  extends(steps: string | string[], option?: { requiredKeys: string[] | string }) {
+    let funnelStep: FunnelStepOption<any> | undefined;
+    if (option != null) {
+      const requiredKeys = Array.isArray(option.requiredKeys) ? option.requiredKeys : [option.requiredKeys];
+      const prevFunnelStepOption = this.prevFunnelStepOption;
       funnelStep = {
-        meta: { requiredKeys },
         guard: (data): data is never => {
           if (typeof data !== 'object' || data == null) {
+            return false;
+          }
+          if (prevFunnelStepOption != null && !prevFunnelStepOption.guard(data)) {
             return false;
           }
           for (const key of requiredKeys) {
@@ -93,29 +72,21 @@ class FunnelStepBuilder<
           return true;
         },
       };
-    } else if (option != null) {
-      funnelStep = typeof option === 'function' ? option(this.meta!) : option;
     } else {
       funnelStep = this.prevFunnelStepOption;
     }
     for (const step of Array.isArray(steps) ? steps : [steps]) {
       this.stepMap[step] = funnelStep;
     }
-    if (funnelStep?.meta != null) {
-      this.meta = funnelStep.meta as never;
-    }
     this.prevFunnelStepOption = funnelStep;
     return this as never;
   }
+
   build(): TStepMap {
-    return this.stepMap as never;
+    return this.stepMap as TStepMap;
   }
 }
 
-function isRequiredKeys(meta: unknown): meta is { requiredKeys: string[] | string } {
-  return typeof meta === 'object' && meta != null && 'requiredKeys' in meta;
-}
-
-export function createFunnelSteps<TContext, TMeta = never>(meta?: TMeta) {
-  return new FunnelStepBuilder<TContext, TMeta>(meta);
+export function createFunnelSteps<TContext>() {
+  return new SimpleFunnelStepBuilder<TContext>();
 }
