f889afab9cd0e5c1c8bb2d6835c28042c026430f
devgiljong <calf-love@naver.com>
2024-09-26 00:03:12 +0900

    :sparkles: App router e2e test add

--- apps/app-router/app/page.tsx
+++ apps/app-router/app/page.tsx
@@ -1,5 +1,9 @@
-import { TestAppRouterFunnel } from '~/src/funnel';
-
+'use client';
+import dynamic from 'next/dynamic';
+const TestAppRouterFunnel = dynamic(() =>
+  import('../src/funnel').then(({ TestAppRouterFunnel }) => TestAppRouterFunnel),
+);
 export default function Home() {
+  //A pre-render error occurs in @use-funnel/browser 0.0.5 version.
   return <TestAppRouterFunnel />;
 }
