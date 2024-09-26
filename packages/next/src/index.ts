b5e42ea2031d7b02a3f9baba322f14767fbdbedc
minukang <minukang@toss.im>
2024-09-26 23:44:29 +0900

    fix

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -43,7 +43,7 @@ export const useFunnel = createUseFunnel<NextPageRouteOption>(({ id, initialStat
     }
   }, [_beforeHistories]);
 
-  const currentIndex = history.length;
+  const currentIndex = beforeHistories.length;
 
   return useMemo(
     () => ({
