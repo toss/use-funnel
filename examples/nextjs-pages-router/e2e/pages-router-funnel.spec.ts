ce1a61edffd02b1908041c11d2b7211799accc1c
MinuKang <minukang5874@gmail.com>
2025-03-13 11:58:21 +0900

    fix(next): next router cleanup check querystring without router (#125)
    * fix(next): next router cleanup check querystring without router
    
    * changeset

--- examples/nextjs-pages-router/e2e/pages-router-funnel.spec.ts
+++ examples/nextjs-pages-router/e2e/pages-router-funnel.spec.ts
@@ -5,17 +5,30 @@ const PAGES_ROUTER_LOCAL_URL = 'http://localhost:3101/';
 test('can move the steps of the funnel using history.push.', async ({ page }) => {
   await page.goto(PAGES_ROUTER_LOCAL_URL);
 
-  await expect(page.getByText('start')).toBeVisible();
+  // start
+  await expect(page.getByRole('heading', { name: 'start' })).toBeVisible();
 
   await expect(page.getByRole('button', { name: 'next' })).toBeVisible();
 
   await page.getByRole('button', { name: 'next' }).click();
 
-  await expect(page.getByText('end')).toBeVisible();
+  // middle
+  await expect(page.getByRole('heading', { name: 'sub start' })).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'next' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'next' }).click();
+
+  await expect(page.getByRole('heading', { name: 'sub end' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'next' }).click();
+
+  // end
+  await expect(page.getByRole('heading', { name: 'end' })).toBeVisible();
 
   await page.goBack();
 
-  await expect(page.getByText('start')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'sub end' })).toBeVisible();
 });
 
 test('can move the steps of the funnel using Link.', async ({ page }) => {
@@ -23,5 +36,16 @@ test('can move the steps of the funnel using Link.', async ({ page }) => {
 
   await page.getByRole('link', { name: 'Go Home' }).click();
 
-  await expect(page.getByText('Home')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
+
+  await page.goBack();
+
+  await expect(page.getByRole('heading', { name: 'start' })).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'next' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'next' }).click();
+
+  // middle
+  await expect(page.getByRole('heading', { name: 'sub start' })).toBeVisible();
 });
